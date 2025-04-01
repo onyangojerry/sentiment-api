8143faaf0d2a4ff084e0daec061f0a2ed0bce166
onyangojerry <jerry.onyango.rawlings@gmail.com>
2025-04-01 14:52:11 -0700

    added a get to the main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ import logging
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Sentiment Classification API!"}
+
+
 @app.post("/predict", response_model=SentimentOutput)
 def get_sentiment(data: TextInput):
     logging.info(f"Received text: {data.text}")
